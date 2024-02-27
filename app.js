d8bbb6c2f4730c807a658a1fa061d44eec6fb064
Aman Verma <amanverma@microsoft.com>
2024-02-27 12:33:52 +0530

    Episode 2

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const parent = React.createElement(
-    "div",
-    {id : "parent"},
-    React.createElement(
-        "div",
-        {id : "child"},
-        [React.createElement("h1",{},"I am the h1 tag"),React.createElement("h2",{},"I am the h2 tag")]
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement("div",{id : "parent"},[
+    React.createElement("div",{id : "child"},[
+        React.createElement("h1",{},"I am the h1 tag"),
+        React.createElement("h2",{},"I am the h2 tag")]
     )
-)
+    ])
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
