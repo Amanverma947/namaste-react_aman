e866fa5b8a7265c38707326563a78b9ad1e391df
AmanGituser <vermaman947@gmail.com>
2024-05-17 11:07:41 +0530

    Initial commit for new feature

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const parent = React.createElement(
     React.createElement(
         "div",
         {id : "child"},
-        [React.createElement("h1",{},"I am the h1 tag"),React.createElement("h2",{},"I am the h2 tag")]
+        [React.createElement("h1",{},"I am the h1 latest2 tag"),React.createElement("h2",{},"I am the h2 tag")]
     )
 )
 const root = ReactDOM.createRoot(document.getElementById("root"));
